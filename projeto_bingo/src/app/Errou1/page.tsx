8293cf76a6a487062f9fa2105ede33282b150b2d
GabrielDamacena <damacenag40@gmail.com>
2024-08-04 22:36:10 -0300

    teste

---
+++ projeto_bingo/src/app/Errou1/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function quest() {
+    return(
+        <main>
+            <div>
+                <h1 className="text-black text-3xl">Errouuuuuuuuuuuuuuu</h1>
+            </div>
+        </main>
+    );
+}
---
+++ projeto_bingo/src/app/R1/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function quest() {
+    return(
+        <main>
+            <div>
+                <h1 className="text-black text-3xl">Acertouuuuuuuuuuuuu</h1>
+            </div>
+        </main>
+    );
+}
